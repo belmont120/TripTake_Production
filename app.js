59b1a1a835929f3fa7df6826d0c12466ca739347
Xin Wang <belmont.xwang@gmail.com>
2018-03-06 14:29:34 +1100

    updatePortNo

--- app.js
+++ app.js
@@ -52,6 +52,6 @@ app.use("/",indexRoutes);
 app.get("*", function (req, res) {
     res.redirect("/moments");
 });
-app.listen(3001, process.env.IP, function () {
+app.listen(3000, process.env.IP, function () {
     console.log("The TripTake Server has started!");
 });
